362a95e659439a07b1ac0d69aca37b0c35fef37f
Gustavo Guimaraes <gustavo.ceci95@gmail.com>
2016-11-18 16:54:34 -0200

    Moved back to very naive implementation

--- Gameloop/Gameloop/Game.cs
+++ Gameloop/Gameloop/Game.cs
@@ -8,43 +8,10 @@ namespace Gameloop
         public const int ScreenWidth = 50;
         public const int ScreenHeight = 50;
 
-        class Character
-        {
-            public int Posx = 5;
-            public int Posy = 5;
+        public int Posx = 5;
+        public int Posy = 5;
 
-            public char Sprite = 'O';
-
-            public void Update()
-            {
-                if (Keyboard.IsKeyDown(Key.Up))
-                {
-                    Posy = Posy - 1;
-                }
-
-                if (Keyboard.IsKeyDown(Key.Down))
-                {
-                    Posy = Posy + 1;
-                }
-
-                if (Keyboard.IsKeyDown(Key.Left))
-                {
-                    Posx = Posx - 1;
-                }
-
-                if (Keyboard.IsKeyDown(Key.Right))
-                {
-                    Posx = Posx + 1;
-                }
-            }
-
-            public void Draw(ConsoleCanvas canvas)
-            {
-                canvas.Draw(Sprite, Posx, Posy);
-            }
-        }
-
-        private Character player;
+        public char Sprite = 'O';
 
         public bool Over { get; }
 
@@ -55,17 +22,35 @@ namespace Gameloop
 
         public void Initialize()
         {
-            player = new Character();
+
         }
 
         internal void Update()
         {
-            player.Update();
+        if (Keyboard.IsKeyDown(Key.Up))
+        {
+            Posy = Posy - 1;
         }
+
+        if (Keyboard.IsKeyDown(Key.Down))
+        {
+            Posy = Posy + 1;
+        }
+
+        if (Keyboard.IsKeyDown(Key.Left))
+        {
+            Posx = Posx - 1;
+        }
+
+        if (Keyboard.IsKeyDown(Key.Right))
+        {
+            Posx = Posx + 1;
+        }
+    }
 
         public void Draw(ConsoleCanvas canvas)
         {
-            player.Draw(canvas);
+            canvas.Draw(Sprite, Posx, Posy);
         }
     }
 }
