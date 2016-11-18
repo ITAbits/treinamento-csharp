1d227a857e75872bb4566dba130e03b112b5368b
Gustavo Guimaraes <gustavo.ceci95@gmail.com>
2016-11-18 15:59:04 -0200

    Added game loop in console thing

---
+++ Gameloop/Gameloop/Game.cs
@@ -0,0 +1,26 @@
+﻿namespace Gameloop
+{
+    public class Game
+    {
+        private static int posx;
+        private static int posy;
+
+        public void Initialize()
+        {
+            posx = 5;
+            posy = 5;
+        }
+
+        internal void Update()
+        {
+            posx = (posx + 1) % 10;
+        }
+
+        public void Draw(ConsoleCanvas canvas)
+        {
+            canvas.Draw('@', posx, posy);
+        }
+
+        
+    }
+}
---
+++ Gameloop/Gameloop/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Gameloop
+{
+    public class Program
+    {
+        private const int Fps = 60;
+
+        private static void Main(string[] args)
+        {
+            var canvas = new ConsoleCanvas(10, 10);
+            var game = new Game();
+
+            game.Initialize();
+
+            while (true)
+            {
+                var start = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+                
+                // Very simple game loop
+                game.Update();
+                game.Draw(canvas);
+                canvas.Render();
+
+                // Make fps constant
+                var deltaTime = start - DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+                System.Threading.Thread.Sleep((int)(1000.0f / Fps - deltaTime));
+            }
+        }
+    }
+    
+}
